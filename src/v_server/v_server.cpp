07aa3370704baa570d1f63c65a079f8d5744bda4
Kozlov Victor <kozlovvictorzx@gmail.com>
2024-06-02 19:21:15 +0300

    extracted parse methods

--- src/v_server/v_server.cpp
+++ src/v_server/v_server.cpp
@@ -5,6 +5,7 @@
 #include "../v_hash_table/v_hash_table.h"
 #include "../v_strings/v_strings.h"
 #include "../v_json/v_json.h"
+#include "v_server.h"
 
 const long TIMEOUT = 500;
 
@@ -32,12 +33,7 @@ void VServer::setup() {
     this->server.begin();
 }
 
-void VServer::listenToNextClient() {
-    WiFiClient client = server.available();
-    if (!client) return;
-    Serial.println("Accepted new connection");
-    
-
+VArrayList<String> VServer::readLines(WiFiClient& client) {
     String currentLine = "";
     VArrayList<String> lines;
     unsigned long lastTime = millis();
@@ -59,38 +55,33 @@ void VServer::listenToNextClient() {
     if (currentLine.length()>0) {
         lines.add(currentLine);
     }
+    return lines;
+}
 
-    for (size_t i = 0;i<lines.size();i++) {
-        String s = lines.getAt(i);
-        Serial.println("---line---");
-        Serial.println(s);
-    }
-
-
-    String method = "";
-    String url = "";
-    VHashTable<String> params;
-
-    if (lines.size()>0) {
-        String firstLine = lines.getAt(0);
-        VArrayList<String> parts = VStrings::splitBy(firstLine,' ');
-        if (parts.size()>=2) {
-            method = parts.getAt(0);
-            url = parts.getAt(1);
-            if (url.indexOf("?")>-1) {
-                VArrayList<String> urlParts = VStrings::splitBy(url,'?');
-                url = urlParts.getAt(0);
-                String queryString = urlParts.getAt(1);
-                VArrayList<String> queryParts = VStrings::splitBy(queryString,'&');
-                for (size_t i = 0;i<queryParts.size();i++) {
-                    VArrayList<String> pair = VStrings::splitBy(queryParts.getAt(i),'=');
-                    params.put(pair.getAt(0),pair.getAt(1));
-                }
+void VServer::parseFirstLine(String firstLine, String *method, String *url, VHashTable<String> *params) {
+    VArrayList<String> parts = VStrings::splitBy(firstLine,' ');
+    if (parts.size()>=2) {
+        *method = parts.getAt(0);
+        *url = parts.getAt(1);
+        if (url->indexOf("?")>-1) {
+            VArrayList<String> urlParts = VStrings::splitBy(*url,'?');
+            *url = urlParts.getAt(0);
+            String queryString = urlParts.getAt(1);
+            VArrayList<String> queryParts = VStrings::splitBy(queryString,'&');
+            for (size_t i = 0;i<queryParts.size();i++) {
+                VArrayList<String> pair = VStrings::splitBy(queryParts.getAt(i),'=');
+                params->put(pair.getAt(0),pair.getAt(1));
             }
         }
     }
+}
 
-    VHashTable<String> headers;
+void VServer::parseLines(VArrayList<String> &lines, String *method, String *url, VHashTable<String> *headers, VHashTable<String> *params)
+{
+    if (lines.size()>0) {
+        this->parseFirstLine(lines.getAt(0), method, url, params);
+    }
+
     boolean isBody = false;
     String bodyRaw = "";
     for (size_t i = 1;i<lines.size();i++) {
@@ -107,26 +98,41 @@ void VServer::listenToNextClient() {
             if (pair.size()>=2) {
                 String val = pair.getAt(1);
                 val.trim();
-                headers.put(pair.getAt(0),val);
+                headers->put(pair.getAt(0),val);
             }
         }
     }
 
     VHashTable<String> body = VJSon::parse(bodyRaw);
     body.forEach([&params](const String key, const String value) {
-        params.put(key,value);
+        params->put(key,value);
     });
+}
+
+void VServer::listenToNextClient() {
+    WiFiClient client = server.available();
+    if (!client) return;
+    Serial.println("Accepted new connection");
+    
+    VArrayList<String> lines = readLines(client);
+
+    String method = "";
+    String url = "";
+    VHashTable<String> headers;
+    VHashTable<String> params;
+
+    this->parseLines(lines, &method, &url, &headers, &params);
 
     Serial.println("Disconnected");
     
     client.println("HTTP/1.1 200 OK");
-    client.println("Content-type:text/html");
+    client.println("Content-type:text/json");
     client.println("Connection: close");
     client.println(); // The HTTP response starts with blank line
     //client.println("{\"done\":true}");
     client.println(method);
     client.println(url);
-    client.println(VJSon::stringify(params));
+    client.print(VJSon::stringify(params));
     client.println(VJSon::stringify(headers));
     client.println(); // The HTTP response ends with another blank line
     client.stop();
--- src/v_server/v_server.h
+++ src/v_server/v_server.h
@@ -3,18 +3,22 @@
 
 #include <Arduino.h>
 #include <WiFi.h>
+#include "../v_array_list/v_array_list.h"
+#include "../v_hash_table/v_hash_table.h"
 
 class VServer {
 private:
   String ssid;
   String password;
   WiFiServer server;
+  VArrayList<String> readLines(WiFiClient &client);
+  void parseFirstLine(String firstLine, String *method, String *url, VHashTable<String> *params);
+  void parseLines(VArrayList<String> &lines, String *method, String *url, VHashTable<String> *headers, VHashTable<String> *params);
 public:
   VServer();
   VServer(String ssid, String password, int port);
   void setup();
   void listenToNextClient();
-
 };
 
 #endif;
